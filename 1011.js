b814419570228ba4aab4f4209291ec1b13a7f006
Tishaa <tishaakter.jui79@gmail.com>
2024-06-19 01:17:11 +0600

    solved 1011

---
+++ 1011.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const radius = parseFloat(lines[0]);
+const pi = 3.14159;
+
+const volume = (4 / 3) * pi * Math.pow(radius, 3);
+
+console.log(`VOLUME = ${volume.toFixed(3)}`);
